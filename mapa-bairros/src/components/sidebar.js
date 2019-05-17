a22436128f266dc4f793fe04806511a071014836
Luan Lima <luan.silva@cipa.com.br>
2019-05-16 22:06:17 -0300

    criando componente de sidebar

--- mapa-bairros/src/components/sidebar.js
+++ mapa-bairros/src/components/sidebar.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import SidebarItem from './sidebarItem';
 
 export default class Sidebar extends Component {
     render() {
@@ -6,51 +7,22 @@ export default class Sidebar extends Component {
             <div className="wrapper">
                 <nav id="sidebar">
                     <div className="sidebar-header">
-                        <h3>Bootstrap Sidebar</h3>
+                        <h3>Principais Atrações</h3>
                     </div>
 
                     <ul className="list-unstyled components">
-                        <p>Dummy Heading</p>
-                        <li className="active">
-                            <a href="#homeSubmenu" data-toggle="collapse" aria-expanded="false" className="dropdown-toggle">Home</a>
-                            <ul className="collapse list-unstyled" id="homeSubmenu">
-                                <li>
-                                    <a href="#">Home 1</a>
-                                </li>
-                                <li>
-                                    <a href="#">Home 2</a>
-                                </li>
-                                <li>
-                                    <a href="#">Home 3</a>
-                                </li>
-                            </ul>
-                        </li>
-                        <li>
-                            <a href="#">About</a>
-                        </li>
-                        <li>
-                            <a href="#pageSubmenu" data-toggle="collapse" aria-expanded="false" className="dropdown-toggle">Pages</a>
-                            <ul class="collapse list-unstyled" id="pageSubmenu">
-                                <li>
-                                    <a href="#">Page 1</a>
-                                </li>
-                                <li>
-                                    <a href="#">Page 2</a>
-                                </li>
-                                <li>
-                                    <a href="#">Page 3</a>
-                                </li>
-                            </ul>
-                        </li>
-                        <li>
-                            <a href="#">Portfolio</a>
-                        </li>
-                        <li>
-                            <a href="#">Contact</a>
-                        </li>
+
+                        <SidebarItem label="Botafogo Praia Shopping" lat="-22.9475308" lng="-43.1834283" ></SidebarItem>
+                        <SidebarItem label="Centro Empresarial Mourisco" lat="-22.9504551" lng="-43.1806529" ></SidebarItem>
+                        <SidebarItem label="Shopping Rio Sul" lat="-22.9574369" lng="-43.1771566" ></SidebarItem>
+                        <SidebarItem label="Fogo de Chão" lat="-22.9490574" lng="-43.180057"></SidebarItem>
+                        <SidebarItem label="Clube Guanabara" lat="-22.8172918" lng="-43.2093807" ></SidebarItem>
+                        <SidebarItem label="Sede de General Severiano(Botafogo de Futebol e Regatas)" lat="-22.9548411" lng="-43.1779454" ></SidebarItem>
+                        <SidebarItem label="Espaço Itaú de Cinema" lat="-22.9450122" lng="-43.182563" ></SidebarItem>
+
+
+
                     </ul>
-
-
 
                 </nav>
 
---
+++ mapa-bairros/src/components/sidebarItem.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+export default class SidebarItem extends Component {
+    
+    render() {
+        return (
+
+        
+                        <li>
+                            <a href="#">{this.props.label}</a>
+                        </li>
+                       
+                   
+
+                    
+
+        )
+    }
+}
