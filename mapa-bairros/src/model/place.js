1ab499464f3bd19542dc63e30ef9ab365f188315
Luan Lima <77luanlima@gmail.com>
2019-05-19 14:14:39 -0300

    Parametrizando venue Id para consultar fotos no foursquare dos locais mapeados.

--- mapa-bairros/src/model/place.js
+++ mapa-bairros/src/model/place.js
@@ -2,9 +2,11 @@ export default class Place
 {
      latitude;
      longitude;
+     foursquareVenueId;
 
-     constructor(latitude, longitude, label)
+     constructor(foursquareVenueId,latitude, longitude, label)
      {
+      this.foursquareVenueId = foursquareVenueId
        this.latitude = latitude;
        this.longitude = longitude;
        this.label = label;  
--- mapa-bairros/src/resource/places.js
+++ mapa-bairros/src/resource/places.js
@@ -2,13 +2,13 @@ import Place from '../model/place';
 
 
 export  const PLACES = [
-    new Place(-22.9475308,-43.1834283,"Botafogo Praia Shopping"),
-    new Place(-22.9504551,-43.1806529,"Centro Empresarial Mourisco"),
-    new Place(22.9574369,43.1771566,"Shopping Rio Sul"),
-    new Place(-22.9490574,-43.180057,"Fogo de Chão"),
-    new Place(-22.8172918,-43.2093807,"Clube Guanabara"),
-    new Place(-22.9548411,-43.1779454,"Sede de General Severiano(Botafogo de Futebol e Regatas)"),
-    new Place(-22.9450122, -43.182563,"Espaço Itaú de Cinema"),
+    new Place("55b106c6498ef5a321195d40",-22.9475308,-43.1834283,"Botafogo Praia Shopping"),
+    new Place("4b7147fcf964a520413f2de3",-22.9504551,-43.1806529,"Centro Empresarial Mourisco"),
+    new Place("4b05872af964a520008322e3",-22.9574369,-43.1771566,"Shopping Rio Sul"),
+    new Place("4e027290483bb38cf0efddf8",-22.9490574,-43.180057,"Fogo de Chão"),
+    new Place("4bd3db3b41b9ef3bd63101e6",-22.9498815, -43.1795691,"Clube Guanabara"),
+    new Place("4bcc763cfb84c9b6ea5f203e",-22.9548411,-43.1779454,"Sede de General Severiano(Botafogo de Futebol e Regatas)"),
+    new Place("4b058729f964a520cc8222e3",-22.9450122, -43.182563,"Espaço Itaú de Cinema"),
 
 
 ];
