1df9efed2eff127d1d2f6174cbeeb83184a1aabb
luan.silva+1-at-514001089657 <luan.silva@CIPA01.LOCAL>
2019-05-24 17:20:45 -0300

    teste commit

--- mapa-bairros/src/components/header.js
+++ mapa-bairros/src/components/header.js
@@ -7,7 +7,6 @@ export default class Header extends Component {
                 <h1 class="header-title">Mapa de Botafogo</h1>
                 <p class="text">Um mapa completo com as principais atrações do bairro Botafogo, no Rio de Janeiro</p>
             </div>
-
         )
     }
 }
