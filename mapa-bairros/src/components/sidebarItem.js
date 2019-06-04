b815f0fca5df5fb2ab82b60f16e66b8a75c963c4
Luan Lima <luan.silva@cipa.com.br>
2019-06-03 21:31:56 -0300

    APlicando alguns coneitos ARIA

--- mapa-bairros/src/components/sidebarItem.js
+++ mapa-bairros/src/components/sidebarItem.js
@@ -1,20 +1,20 @@
 import React, { Component } from 'react';
 
 export default class SidebarItem extends Component {
-    
+
 
     render() {
         return (
 
-        
-                        <li className ="sidebarLink">
-                            <a  href="#" onClick={() => this.props.filterMap(this.props.label)}
-                             >{this.props.label}</a>
-                        </li>
-                       
-                   
 
-                    
+            <li className="sidebarLink">
+                <a role="menuitem" href="#" onClick={() => this.props.filterMap(this.props.label)}
+                >{this.props.label}</a>
+            </li>
+
+
+
+
 
         )
     }
