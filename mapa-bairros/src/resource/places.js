5df5f4868a7b0d3a531f246ed90f377fee4bd185
luan.silva+1-at-514001089657 <luan.silva@CIPA01.LOCAL>
2019-06-06 10:29:11 -0300

    Ajustando nome estabelecimento

--- mapa-bairros/src/resource/places.js
+++ mapa-bairros/src/resource/places.js
@@ -2,7 +2,7 @@ import Place from '../model/place';
 
 
 export  var PLACES = [
-    new Place("55b106c6498ef5a321195d40",-22.9475308,-43.1834283,"Botafogo Praia Shopping"),
+    new Place("55b106c6498ef5a321195d40",-22.9475308,-43.1834283,"Chocolataria Lindt"),
     new Place("4b7147fcf964a520413f2de3",-22.9504551,-43.1806529,"Centro Empresarial Mourisco"),
     new Place("4b05872af964a520008322e3",-22.9574369,-43.1771566,"Shopping Rio Sul"),
     new Place("4e027290483bb38cf0efddf8",-22.9490574,-43.180057,"Fogo de Chão"),
